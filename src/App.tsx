7944d63b0e594c20fe82c1e776dc6b51c3d14747
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2025-08-30 14:39:17 +0100

    Create basic project structure and add main dependencies -tailwindcss, react-router-dom, leaflet

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <main>
+          <Routes></Routes>
+        </main>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
