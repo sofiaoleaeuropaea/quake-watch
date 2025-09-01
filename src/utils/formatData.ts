c2958ebbfe86f87392fa043cbbb594d263b3e4d3
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2025-09-01 19:29:22 +0100

    refactor(utils): move `formatCoordinates` to dedicated `utils/formatData.ts'

--- src/utils/formatData.ts
+++ src/utils/formatData.ts
@@ -3,3 +3,15 @@ import { format } from 'date-fns';
 export const formatDate = (timestamp: number): string => {
   return format(new Date(timestamp), 'MMM d, yyyy HH:mm');
 };
+
+export const formatCoordinates = (
+  latitude: number,
+  longitude: number,
+): string => {
+  const latitudeDir = latitude >= 0 ? 'N' : 'S';
+  const longitudeDir = longitude >= 0 ? 'E' : 'W';
+
+  return `${Math.abs(latitude).toFixed(4)}°${latitudeDir}, ${Math.abs(
+    longitude,
+  ).toFixed(4)}°${longitudeDir}`;
+};
