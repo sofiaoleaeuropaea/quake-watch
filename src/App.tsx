5a798caa80e49941316331480a354065ee8b71c2
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2025-09-01 01:00:17 +0100

    feature(navbar): integrate Navbar with React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import Navbar from './components/Navbar';
 const App = () => {
   return (
     <>
       <BrowserRouter>
+      <Navbar/>
         <main>
           <Routes>
             <Route path='/quake-watch' element={<Dashboard/>} />
