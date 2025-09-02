facdef5b752fb31d1c2a24f1480994a6f158a422
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2025-09-02 03:12:52 +0100

    chore: change to gemini-1-5-flash to reduce cost

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -27,7 +27,7 @@ const normalizePromptFilters = (obj: any): PromptFilters => {
 export const parsePromptWithGemini = async (prompt: string): Promise<PromptFilters> => {
   const geminikey = import.meta.env.VITE_GEMINI_API_KEY!;
   const res = await fetch(
-    'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent',
+    'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent',
     {
       method: 'POST',
       headers: {
