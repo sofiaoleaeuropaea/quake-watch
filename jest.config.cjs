944174e51547ba8a95e5161048f952b7f844552e
Sofia Oliveira <sofia.oliveira57.so@gmail.com>
2025-10-06 22:35:17 +0100

    build(jest): add ts-node to support jest.config.ts

--- jest.config.cjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
-  transform: { '^.+\\.(ts|tsx)$': ['ts-jest', { tsconfig: 'tsconfig.json' }] },
-  moduleNameMapper: { '\\.(css|less|sass|scss)$': 'identity-obj-proxy' },
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/build/'],
-};
